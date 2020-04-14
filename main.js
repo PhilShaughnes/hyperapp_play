89578f5d3fb539b6ec90579cd57cb234875c9136
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2020-04-13 21:04:28 -0400

    Add buttons and states
    This is basically the state machine, hooked up to a view, that depends
    on the state, as well as sending actions to the state machine on clicks.
    cool!

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 import { h, app } from "https://unpkg.com/hyperapp"
 
 const Start = state =>
-    state.mode === "stopped"
-    ? { ...state, mode: "running" }
-    : state
+  state.mode === "stopped"
+  ? { ...state, mode: "running" }
+  : state
 const Pause = state =>
-    state.mode === "running"
-    ? { ...state, mode: "paused" }
-    : state
+  state.mode === "running"
+  ? { ...state, mode: "paused" }
+  : state
 const Continue = state =>
-    state.mode === "paused"
-    ? { ...state, mode: "running" }
-    : state
+  state.mode === "paused"
+  ? { ...state, mode: "running" }
+  : state
 const Cancel = state => ({ ...state, mode: "stopped" })
 
 app({
-  init: { mode: "paused" },
-  view: state =>
-    h("div", {}, [
-      h("p", {}, [`Current state: ${state.mode}`]),
+  init: { mode: "stopped" },
+  view: state => h("div", {}, [
+    h("p", {}, [
+      state.mode == "stopped"
+      ? h("button", { onclick: Start }, "START")
+      : h("button", { onclick: Cancel }, "CANCEL"),
+
+      state.mode === "paused"
+      ? h("button", { onclick: Continue }, "CONTINUE")
+      : h(
+        "button",
+        {
+          disabled: state.mode === "stopped",
+          onclick: Pause
+        },
+        "PAUSE"
+      )
     ]),
+    h("p", {}, [`Current state: ${state.mode}`]),
+  ]),
   node: document.getElementById("app")
 })
 
