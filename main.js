b98099ab189649935e929a31e8683a2366c174f3
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2020-04-15 20:37:07 -0400

    Abstract views for generalization

--- main.js
+++ main.js
@@ -57,46 +57,54 @@ const onAnimationFrame = (() => {
     return action => [subFn, { action }]
 })()
 
+const Controls = state => h("p", {}, [
+  state.mode == "stopped"
+  ? h("button", { onclick: Start }, "START")
+  : h("button", { onclick: Cancel }, "CANCEL"),
+
+  state.mode === "paused"
+  ? h("button", { onclick: Continue }, "CONTINUE")
+  : h(
+    "button",
+    {
+      disabled: state.mode === "stopped",
+      onclick: Pause
+    },
+    "PAUSE"
+  )
+])
+
+const Gauge = state => h("div", { class: "gauge" }, [
+  h("div", {
+    class: "gauge-meter",
+    style: {
+      width:
+        state.mode !== "stopped"
+          ? (100 * state.remainingTime) / DURATION + "%"
+          : "100%"
+    }
+  }),
+  state.mode !== "stopped" &&
+  h("p", { class: "gauge-text" }, [
+    Math.ceil(state.remainingTime / 1000),
+    " s"
+  ])
+])
+
 app({
   init: { mode: "stopped" },
   subscriptions: state => [
     state.mode === "running" && onAnimationFrame(UpdateTime)
   ],
   view: state => h("div", {}, [
-    h("p", {}, [
-      state.mode == "stopped"
-      ? h("button", { onclick: Start }, "START")
-      : h("button", { onclick: Cancel }, "CANCEL"),
-
-      state.mode === "paused"
-      ? h("button", { onclick: Continue }, "CONTINUE")
-      : h(
-        "button",
-        {
-          disabled: state.mode === "stopped",
-          onclick: Pause
-        },
-        "PAUSE"
-      )
-    ]),
     h("p", {}, [`Current state: ${state.mode}`]),
-
-    h("div", { class: "gauge" }, [
-      h("div", {
-        class: "gauge-meter",
-        style: {
-          width:
-            state.mode !== "stopped"
-              ? (100 * state.remainingTime) / DURATION + "%"
-              : "100%"
-        }
-      }),
-      state.mode !== "stopped" &&
-      h("p", { class: "gauge-text" }, [
-        Math.ceil(state.remainingTime / 1000),
-        " s"
-      ])
-    ])
+    h("p", {}, "timer 1: "),
+    Controls(state),
+    Gauge(state),
+    h("hr"),
+    h("p", {}, "timer 2: "),
+    Controls(state),
+    Gauge(state)
   ]),
   node: document.getElementById("app")
 })
